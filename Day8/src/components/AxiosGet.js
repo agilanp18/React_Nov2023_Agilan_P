e4035c6cc54bb3b7e8f888836ab3c9f27aa1c3d7
agilanp18 <124856152+agilanp18@users.noreply.github.com>
2023-11-23 12:29:28 +0530

    Add files via upload

---
+++ Day8/src/components/AxiosGet.js
@@ -0,0 +1,31 @@
+import React,{useEffect,useState} from "react";
+import axios from "axios";
+function AxiosGet(){
+    const[employees,setEmployees]=useState([]);
+    useEffect(()=>{
+
+        axios
+        .get('http://localhost:3002/users')
+        .then((res)=>
+        {
+            setEmployees(res.data);
+        })
+    .catch((err)=>
+    {
+        console.log(err);
+    })
+    },[]);
+
+return(
+    <div>
+        AxiosGet
+        <ul>
+            {employees.map((employee)=>
+            (
+                <li key={employee.id}>{employee.name}</li>
+            ))}
+        </ul>
+    </div>
+)
+}
+export default AxiosGet;
