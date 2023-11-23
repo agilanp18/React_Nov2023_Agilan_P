787295ef03d8e231ac03e7ca7adb58c9ee8ba437
agilanp18 <124856152+agilanp18@users.noreply.github.com>
2023-11-23 12:24:26 +0530

    Day2

---
+++ Day6/reactapp1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Preson1 from './components/Preson1';
+import Person2 from './components/Person2';
+function App() {
+  return (
+    <div className="App">
+      <Preson1/>
+      <Person2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day6/reactapp1/src/components/Person2.js
@@ -0,0 +1,11 @@
+import updatedComponent from "./HOC";
+function Person2({money,handleIncrease})
+{
+    return (
+        <div>
+            <h2>Mike is offering ${money}</h2>
+            <button onClick={handleIncrease}>Increase Offering</button>
+        </div>
+    );
+}
+export default updatedComponent(Person2);
